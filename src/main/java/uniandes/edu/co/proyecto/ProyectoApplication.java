170c302357632e2b594ca17748e276428a416691
Manuel Prieto <m.prietoh@uniandes.edu.co>
2024-03-17 15:59:37 -0500

    Puntos de atencion se agregan

--- src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/proyecto/ProyectoApplication.java
@@ -1,15 +1,32 @@
 package uniandes.edu.co.proyecto;
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import uniandes.edu.co.proyecto.modelo.Punto_atencion;
+import uniandes.edu.co.proyecto.repositorio.Punto_atencionRepository;
 
 
 
 
 @SpringBootApplication
-public class ProyectoApplication {
+public class ProyectoApplication implements CommandLineRunner {
 
+	@Autowired
+	private Punto_atencionRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}	
+
+	@Override
+	public void run(String...args){
+		Collection<Punto_atencion> puntos = repository.darPuntos_atencion();
+		for(Punto_atencion punto : puntos){
+			System.out.println(punto);
+		}
+	}
 
 }
--- src/main/java/uniandes/edu/co/proyecto/modelo/Punto_atencion.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Punto_atencion.java
@@ -4,53 +4,55 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="puntos_atencion")
-
-
 public class Punto_atencion {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
+    private String tipo_punto;
+
     private String direccion;
-    private String tipoPunto;
-    
 
-    public Punto_atencion(String direccion, String tipoPunto) {
+    public Punto_atencion(){;}
+
+    public Punto_atencion(String tipo_punto, String direccion)
+    {
+        this.tipo_punto = tipo_punto;
         this.direccion = direccion;
-        this.tipoPunto = tipoPunto;
     }
-
-    public Punto_atencion()
-    {}
 
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTipo_punto() {
+        return tipo_punto;
+    }
+
+    public void setTipo_punto(String tipo_punto) {
+        this.tipo_punto = tipo_punto;
     }
 
     public String getDireccion() {
         return direccion;
     }
 
-    public String getTipoPunto() {
-        return tipoPunto;
-    }
-
-    public void setId(Integer idPunto) {
-        this.id = idPunto;
-    }
-
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
 
-    public void setTipoPunto(String tipoPunto) {
-        this.tipoPunto = tipoPunto;
-    }   
-
+    @Override
+    public String toString()
+    {
+        return this.tipo_punto+"|"+this.direccion;
+    }
     
 }
