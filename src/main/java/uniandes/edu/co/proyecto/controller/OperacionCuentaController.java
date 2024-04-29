3518a25f996387cd2ae9f20065fa7301e31028c3
Miguel Ariza <m.arizaj@uniandes.edu.co>
2024-04-28 20:08:08 -0500

    Transaccionalidad RF6

--- src/main/java/uniandes/edu/co/proyecto/controller/OperacionCuentaController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/OperacionCuentaController.java
@@ -9,21 +9,30 @@ import org.springframework.ui.Model;
 import uniandes.edu.co.proyecto.modelo.Cuenta;
 import uniandes.edu.co.proyecto.modelo.OperacionCuenta;
 import uniandes.edu.co.proyecto.repositorio.OperacionCuentaRepository;
+import uniandes.edu.co.proyecto.servicios.OperacionCuentaService;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OperacionCuentaController {
     @Autowired
     private OperacionCuentaRepository operacionCuentaRepository;
 
+    @Autowired
+    private OperacionCuentaService operacionCuentaService;
+
     @GetMapping("/operaciones_cuenta")
     public String operacionesCuenta(Model model) {
-        model.addAttribute("operaciones_cuenta", operacionCuentaRepository.darOperacionesCuenta());
+        try {
+            model.addAttribute("operaciones_cuenta", operacionCuentaService.consultarOperacionesConFantasma());
+        } catch (Exception e) {
+        }
+        
         return "operacionesCuenta";
     }
 
@@ -56,6 +65,23 @@ public class OperacionCuentaController {
         return "redirect:/operaciones_cuenta";
     }
 
+    @GetMapping("/operaciones_cuenta/{id}/edit/wait")
+    public String operacionCuentaEditarEsperaForm(@PathVariable("id") Integer id, Model model) {
+        OperacionCuenta operacion = operacionCuentaRepository.darOperacionCuenta(id);
+        if(operacion != null){
+            model.addAttribute("operacion_cuenta", operacion);
+            return "operacionCuentaEditarEspera";
+        } else {
+            return "redirect:/operaciones_cuenta";
+        }
+    }
+
+    @PostMapping("/operaciones_cuenta/{id}/edit/save/wait")
+    public String operacionCuentaEditarGuardarEspera(@PathVariable("id") Integer id, @ModelAttribute OperacionCuenta operacion) {
+        operacionCuentaService.actualizar_operacionCuenta_con_espera(id, operacion.getCuenta(), operacion.getDetalle());;
+        return "redirect:/operaciones_cuenta";
+    }
+
     @GetMapping("/operaciones_cuenta/{id}/delete")
     public String operacionCuentaEliminar(@PathVariable("id") Integer id) {
         operacionCuentaRepository.eliminarOperacionCuenta(id);
@@ -67,5 +93,18 @@ public class OperacionCuentaController {
         Collection<OperacionCuenta> operaciones = operacionCuentaRepository.findByCuenta(cuenta);
         model.addAttribute("extracto", operaciones);
         return "extracto"; // Vista para cuentas filtradas por tipo
+    }
+
+    @GetMapping("/operaciones_cuenta/bloqueo")
+    public String consultarOperacionesSinFantasma(RedirectAttributes redirectAttributes) {
+        try {
+            // Indicar el ID del bar a consultar.
+            operacionCuentaService.consultarOperacionesSinFantasma();
+        } catch (Exception e) {
+            System.err.println("Error durante la consulta de operaciones: " + e.getMessage());
+            redirectAttributes.addFlashAttribute("errorMessage", "No se pudo consultar las operaciones correctamente.");
+            return "redirect:/operaciones_cuenta";
+        }
+        return "redirect:/operaciones_cuenta";
     }
 }
